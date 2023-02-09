6d5ad9f1a29515ec821aaea7949d9ba7f96b6e3e
glmsr12 <gulumsercakmak@hotmail.com>
2023-02-08 19:54:33 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const reviewContainer = document.getElementById(review - container);
+
+const reviews = [
+  {
+    img: 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
+    alt: 'profile1',
+    text: 'Course has really helped our business. Needless to say we are extremely satisfied with the results. Course is great. Keep up the excellent work.',
+    name: 'Mersey K.',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
+    alt: 'profile1',
+    text: 'Course has really helped our business. Needless to say we are extremely satisfied with the results. Course is great. Keep up the excellent work.',
+    name: 'Mersey K.',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
+    alt: 'profile1',
+    text: 'Course has really helped our business. Needless to say we are extremely satisfied with the results. Course is great. Keep up the excellent work.',
+    name: 'Mersey K.',
+  },
+];
