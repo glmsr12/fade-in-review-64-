7c71fb587ef9bd7d19fbb0051df2eccf8aae7e8f
glmsr12 <gulumsercakmak@hotmail.com>
2023-02-11 20:54:23 -0800

    "cards added on review container"

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const reviewContainer = document.getElementById(review - container);
+const reviewContainer = document.getElementById('review-container');
 
 const reviews = [
   {
@@ -8,15 +8,24 @@ const reviews = [
     name: 'Mersey K.',
   },
   {
-    img: 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
-    alt: 'profile1',
-    text: 'Course has really helped our business. Needless to say we are extremely satisfied with the results. Course is great. Keep up the excellent work.',
-    name: 'Mersey K.',
+    img: 'https://images.unsplash.com/photo-1531123414780-f74242c2b052?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
+    alt: 'profile2',
+    text: 'No matter where you go, course is the coolest, most happening thing around! Thanks for the great service.',
+    name: 'Jerrine G.',
   },
   {
-    img: 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
-    alt: 'profile1',
-    text: 'Course has really helped our business. Needless to say we are extremely satisfied with the results. Course is great. Keep up the excellent work.',
-    name: 'Mersey K.',
+    img: 'https://images.unsplash.com/photo-1544723795-3fb6469f5b39?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80',
+    alt: 'profile3',
+    text: 'Since I invested in course I made over 100,000 dollars profits. I use course often. Needless to say we are extremely satisfied with the results.',
+    name: 'Wolf T.',
   },
 ];
+
+function populateDisplay() {
+  reviews.forEach((review) => {
+    const cardElement = document.createElement('div'); //create div for the profile cards
+    cardElement.classList.add('card'); // add class to style it
+    reviewContainer.append(cardElement);
+  });
+}
+populateDisplay();
